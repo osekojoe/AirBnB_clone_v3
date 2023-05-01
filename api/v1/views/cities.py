192949a1cb104f12a5209539dc9fd498ca3e8f95
jdev <20902932+osekojoe@users.noreply.github.com>
2023-05-01 12:30:41 +0000

    pep8 style fix

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -22,6 +22,7 @@ def get_cities(state_id):
 
     return jsonify(cities)
 
+
 @app_views.route('/cities/<city_id>', methods=['GET'], strict_slashes=False)
 def get_city(city_id):
     """"""
@@ -30,6 +31,7 @@ def get_city(city_id):
         abort(404)
 
     return jsonify(city.to_dict())
+
 
 @app_views.route('/cities/<city_id>', methods=['DELETE'], strict_slashes=False)
 def delete_city(city_id):
@@ -43,7 +45,9 @@ def delete_city(city_id):
 
     return make_response(jsonify({}), 200)
 
-@app_views.route('/states/<state_id>/cities', methods=['POST'], strict_slashes=False)
+
+@app_views.route('/states/<state_id>/cities', methods=['POST'],
+                 strict_slashes=False)
 def create_city(state_id):
     """"""
     state = storage.get(State, state_id)
@@ -63,7 +67,8 @@ def create_city(state_id):
 
     return make_response(jsonify(city.to_dict()), 201)
 
-@app_views.route('/api/v1/cities/<city_id>', methods=['PUT'], strict_slashes=False)
+
+@app_views.route('/cities/<city_id>', methods=['PUT'], strict_slashes=False)
 def update_city(city_id):
     """"""
     city = storage.get(City, city_id)
@@ -72,7 +77,7 @@ def update_city(city_id):
 
     data = request.get_json()
     if not data:
-         abort(400, description="Not a JSON")
+        abort(400, description="Not a JSON")
 
     ignore_keys = ['id', 'state_id', 'created_at', 'updated_at']
 
