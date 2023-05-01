2fcaf11afb632088878e87f0cb11fa9583f75c10
jdev <20902932+osekojoe@users.noreply.github.com>
2023-05-01 12:22:45 +0000

    city - RESTFul API actions

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -8,7 +8,7 @@ from api.v1.views import app_views
 from models import storage
 from models.state import State
 from models.city import City
-from flask import jsonify, make_response request, abort
+from flask import abort, jsonify, make_response, request
 
 
 @app_views.route('/states/<state_id>/cities', methods=['GET'])
@@ -18,9 +18,9 @@ def get_cities(state_id):
     if not state:
         abort(404)
 
-   cities = [city.to_dict() for city in state.cities]
+    cities = [city.to_dict() for city in state.cities]
 
-   return jsonify(cities)
+    return jsonify(cities)
 
 @app_views.route('/cities/<city_id>', methods=['GET'], strict_slashes=False)
 def get_city(city_id):
@@ -50,27 +50,27 @@ def create_city(state_id):
     if not state:
         abort(404)
 
-   data = request.get_json()
-   if not data:
-       abort(400, 'Not a JSON')
+    data = request.get_json()
+    if not data:
+        abort(400, 'Not a JSON')
 
-   if 'name' not in data:
-       abort(400, 'Missing name')
+    if 'name' not in data:
+        abort(400, 'Missing name')
 
-   city = City(**data)
-   city.state_id = state_id
-   city.save()
+    city = City(**data)
+    city.state_id = state_id
+    city.save()
 
-   return make_response(jsonify(city.to_dict()), 201)
+    return make_response(jsonify(city.to_dict()), 201)
 
-@app.route('/api/v1/cities/<city_id>', methods=['PUT'], strict_slashes=False)
+@app_views.route('/api/v1/cities/<city_id>', methods=['PUT'], strict_slashes=False)
 def update_city(city_id):
     """"""
     city = storage.get(City, city_id)
     if not city:
         abort(404)
 
-    data = request.get_json():
+    data = request.get_json()
     if not data:
          abort(400, description="Not a JSON")
 
