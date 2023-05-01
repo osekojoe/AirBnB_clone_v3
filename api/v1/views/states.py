c3204de00ac7a99f5eeca0bc29436b36db35c286
jdev <20902932+osekojoe@users.noreply.github.com>
2023-05-01 12:37:52 +0000

    State - RESTFul API actions

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -28,7 +28,8 @@ def get_state(state_id):
     return jsonify(state.to_dict())
 
 
-@app_views.route('/states/<state_id>', methods=['DELETE'], strict_slashes=False)
+@app_views.route('/states/<state_id>', methods=['DELETE'],
+                 strict_slashes=False)
 def delete_state(state_id):
     """ DELETE """
     state = storage.get(State, state_id)
