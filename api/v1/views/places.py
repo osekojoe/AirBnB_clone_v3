dbcb8a25806892bd5d6e9a5e6e4270ccee271d35
jdev <20902932+osekojoe@users.noreply.github.com>
2023-05-01 17:20:54 +0000

    Places - Restful API actions

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -1,19 +1,20 @@
 #!/usr/bin/python3
-""" objects that handle all default RestFul API actions for Places """
+"""
+
+"""
+
+
+from api.v1.views import app_views
+from models import storage
 from models.state import State
 from models.city import City
 from models.place import Place
 from models.user import User
-from models.amenity import Amenity
-from models import storage
-from api.v1.views import app_views
 from flask import abort, jsonify, make_response, request
-from flasgger.utils import swag_from
 
 
 @app_views.route('/cities/<city_id>/places', methods=['GET'],
                  strict_slashes=False)
-@swag_from('documentation/place/get_places.yml', methods=['GET'])
 def get_places(city_id):
     """
     Retrieves the list of all Place objects of a City
@@ -29,7 +30,6 @@ def get_places(city_id):
 
 
 @app_views.route('/places/<place_id>', methods=['GET'], strict_slashes=False)
-@swag_from('documentation/place/get_place.yml', methods=['GET'])
 def get_place(place_id):
     """
     Retrieves a Place object
@@ -43,7 +43,6 @@ def get_place(place_id):
 
 @app_views.route('/places/<place_id>', methods=['DELETE'],
                  strict_slashes=False)
-@swag_from('documentation/place/delete_place.yml', methods=['DELETE'])
 def delete_place(place_id):
     """
     Deletes a Place Object
@@ -62,7 +61,6 @@ def delete_place(place_id):
 
 @app_views.route('/cities/<city_id>/places', methods=['POST'],
                  strict_slashes=False)
-@swag_from('documentation/place/post_place.yml', methods=['POST'])
 def post_place(city_id):
     """
     Creates a Place
@@ -94,7 +92,6 @@ def post_place(city_id):
 
 
 @app_views.route('/places/<place_id>', methods=['PUT'], strict_slashes=False)
-@swag_from('documentation/place/put_place.yml', methods=['PUT'])
 def put_place(place_id):
     """
     Updates a Place
